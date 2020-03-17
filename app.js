aa0973db5b073f357b20ae2f3cb979ca5d097d02
prasetiodc <mprasetiodc@gmail.com>
2020-03-17 23:02:02 +0700

    change route static

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ let app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
-app.use('/file', express.static('uploads'))
+app.use('/uploads', express.static('uploads'))
 app.use(morgan('dev'))
 
 app.use('/', route)
