1a9d4774cffcdb276bfd3ce6a338a11c2dc36c8c
prasetiodc <mprasetiodc@gmail.com>
2020-08-31 20:16:25 +0700

    add route import visit

--- controllers/visit.js
+++ controllers/visit.js
@@ -3,6 +3,7 @@ const Op = require('sequelize').Op
 var Jimp = require('jimp');
 const fs = require('fs')
 const { createDateAsUTC } = require('../helpers/convertDate');
+const excelToJson = require('convert-excel-to-json');
 
 class visit {
   static async create(req, res) {
@@ -401,6 +402,203 @@ class visit {
       console.log(err)
       if (err === "bad request") res.status(400).json({ message: "Bad request" })
       else res.status(500).json({ message: "Error", err })
+    }
+  }
+
+  static async import(req, res) {
+    try {
+      console.log("MASUK")
+      let result = excelToJson({
+        sourceFile: req.file.path,
+        header: {
+          rows: 2
+        },
+        columnToKey: {
+          A: 'retailer',
+          B: 'storeName',
+          C: 'storeCode',
+          D: 'bhnStore',
+          E: 'dc',
+          F: 'dateOfVisit',
+          G: 'address',
+          H: 'district',
+          I: 'city',
+          J: 'activeGift',
+          K: 'activePOR',
+          L: 'redemption',
+          M: '50kFacings',
+          N: '100kFacings',
+          O: '150kFacings',
+          P: '300kFacings',
+          Q: '500kFacings',
+          R: 'entryFixtureType',
+          S: 'entryBrokenPegs',
+          T: 'entryPOG',
+          U: 'entryCorrectPOG',
+          V: 'entry50kFacing',
+          W: 'entry100kFacing',
+          X: 'entry150kFacing',
+          Y: 'entry300kFacing',
+          Z: 'entry500kFacing',
+          AA: 'entryGoogle50k',
+          AB: 'entryGoogle100k',
+          AC: 'entryGoogle150k',
+          AD: 'entryGoogle300k',
+          AE: 'entryGoogle500k',
+          AF: 'entrySpotify1m',
+          AG: 'entrySpotify3m',
+          AH: 'entryPOPPic1',
+          AI: 'entryPOPPic2',
+          AJ: 'exitFixtureType',
+          AK: 'exitBrokenPegs',
+          AL: 'exitPOG',
+          AM: 'exitCorrectPOG',
+          AN: 'exit50kFacing',
+          AO: 'exit100kFacing',
+          AP: 'exit150kFacing',
+          AQ: 'exit300kFacing',
+          AR: 'exit500kFacing',
+          AS: 'exitGoogle50k',
+          AT: 'exitGoogle100k',
+          AU: 'exitGoogle150k',
+          AV: 'exitGoogle300k',
+          AW: 'exitGoogle500k',
+          AX: 'exitSpotify1m',
+          AY: 'exitSpotify3m',
+          AZ: 'exitPOPPic1',
+          BA: 'exitPOPPic2',
+          BB: 'md'
+        }
+      })
+      res.status(200).json({ message: "Success" })
+
+      let visited = await tbl_visits.findAll({ limit: 1, offset: 1 })
+      let allMD = await tbl_users.findAll()
+      let allFixtureType = await tbl_fixture_types.findAll()
+      let allStore = await tbl_stores.findAll()
+      let sheet
+
+      for (let key in result) {
+        sheet = result[key]
+      }
+
+      console.log("LENGTH OF DATA", sheet.length)
+      console.log("START IMPORT VISIT", createDateAsUTC(new Date()))
+      for (let i = 0; i < sheet.length; i++) {
+        let storeSelected = await allStore.find(store => store.store_code === sheet[i].storeCode)
+        let entryFixComp, entryCorrectPOG, exitFixComp, exitCorrectPOG
+
+        if (sheet[i].entryFixtureType === "Vertical Inline") {
+          if (storeSelected.fixture_type_id_1 === 2 || storeSelected.fixture_type_id_1 === 3) { //DEFAULT VERTICAL INLINE
+            entryFixComp = 1
+            entryCorrectPOG = null
+          } else { //BUKAN DEFAULT VERTICAL INLINE
+            if (storeSelected.retailer_id === 1) { //IDM
+              entryFixComp = 0
+              entryCorrectPOG = 2
+            } else {
+              entryFixComp = 0
+              entryCorrectPOG = 3
+            }
+          }
+        } else {
+          let checkFixtureType = await allFixtureType.find(fixType => fixType.fixture_type.toLowerCase() === sheet[i].entryFixtureType.toLowerCase())
+
+          if (storeSelected.fixture_type_id_1 === checkFixtureType.id) {
+            entryFixComp = 1
+            entryCorrectPOG = null
+          } else {
+            entryFixComp = 0
+            entryCorrectPOG = checkFixtureType.id
+          }
+        }
+
+        if (sheet[i].exitFixtureType === "Vertical Inline") {
+          if (storeSelected.fixture_type_id_1 === 2 || storeSelected.fixture_type_id_1 === 3) { //DEFAULT VERTICAL INLINE
+            exitFixComp = 1
+            exitCorrectPOG = null
+          } else { //BUKAN DEFAULT VERTICAL INLINE
+            if (storeSelected.retailer_id === 1) { //IDM
+              exitFixComp = 0
+              exitCorrectPOG = 2
+            } else {
+              exitFixComp = 0
+              exitCorrectPOG = 3
+            }
+          }
+        } else {
+          let checkFixtureType = await allFixtureType.find(fixType => fixType.fixture_type.toLowerCase() === sheet[i].exitFixtureType.toLowerCase())
+
+          if (storeSelected.fixture_type_id_1 === checkFixtureType.id) {
+            exitFixComp = 1
+            exitCorrectPOG = null
+          } else {
+            exitFixComp = 0
+            exitCorrectPOG = checkFixtureType.id
+          }
+        }
+
+        let newData = {
+          img_store: visited[0].img_store,
+          img_fixture_in: visited[0].img_fixture_in,
+          img_fixture_out: visited[0].img_fixture_out,
+          visit_date: sheet[i].dateOfVisit,
+          user_id: allMD.find(md => md.nik === sheet[i].md || md.name === sheet[i].md).id,
+          store_code: sheet[i].storeCode,
+          entry_fixture_comp: entryFixComp,
+          entry_correct_fixture: entryCorrectPOG,
+          entry_peg_comp: sheet[i].entryBrokenPegs === 0 ? 1 : 0,
+          entry_broken_hanger: sheet[i].entryBrokenPegs === 0 ? null : sheet[i].entryBrokenPegs,
+          entry_pog_comp: sheet[i].entryCorrectPOG ? 0 : 1,
+          entry_correct_pog: sheet[i].entryCorrectPOG || null,
+          entry_pop_pic_1: sheet[i].entryPOPPic1 === "Yes" ? 1 : 0,
+          entry_pop_pic_2: sheet[i].entryPOPPic2 === "Yes" ? 1 : 0,
+          entry_google50k: sheet[i].entryGoogle50k === "Over 15" ? 15 : sheet[i].entryGoogle50k,
+          entry_google100k: sheet[i].entryGoogle100k === "Over 15" ? 15 : sheet[i].entryGoogle100k,
+          entry_google150k: sheet[i].entryGoogle150k === "Over 10" ? 10 : sheet[i].entryGoogle150k,
+          entry_google300k: sheet[i].entryGoogle300k === "Over 10" ? 10 : sheet[i].entryGoogle300k,
+          entry_google500k: sheet[i].entryGoogle500k === "Over 10" ? 10 : sheet[i].entryGoogle500k,
+          entry_spotify1M: sheet[i].entrySpotify1m === "Yes" ? 15 : sheet[i].entrySpotify1m,
+          entry_spotify3M: sheet[i].entrySpotify3m === "Yes" ? 15 : sheet[i].entrySpotify3m,
+          exit_fixture_comp: exitFixComp,
+          exit_correct_fixture: exitCorrectPOG,
+          exit_peg_comp: sheet[i].exitBrokenPegs === 0 ? 1 : 0,
+          exit_broken_hanger: sheet[i].exitBrokenPegs === 0 ? null : sheet[i].exitBrokenPegs,
+          exit_pog_comp: sheet[i].exitCorrectPOG ? 0 : 1,
+          exit_correct_pog: sheet[i].exitCorrectPOG || null,
+          exit_pop_pic_1: sheet[i].exitPOPPic1 === "Yes" ? 1 : 0,
+          exit_pop_pic_2: sheet[i].exitPOPPic2 === "Yes" ? 1 : 0,
+          exit_google50k: sheet[i].exitGoogle50k === "Over 15" ? 15 : sheet[i].exitGoogle50k,
+          exit_google100k: sheet[i].exitGoogle100k === "Over 15" ? 15 : sheet[i].exitGoogle100k,
+          exit_google150k: sheet[i].exitGoogle150k === "Over 10" ? 10 : sheet[i].exitGoogle150k,
+          exit_google300k: sheet[i].exitGoogle300k === "Over 10" ? 10 : sheet[i].exitGoogle300k,
+          exit_google500k: sheet[i].exitGoogle500k === "Over 10" ? 10 : sheet[i].exitGoogle500k,
+          exit_spotify1M: sheet[i].exitSpotify1m === "Yes" ? 15 : sheet[i].exitSpotify1m,
+          exit_spotify3M: sheet[i].exitSpotify3m === "Yes" ? 15 : sheet[i].exitSpotify3m,
+          q1: sheet[i].activeGift === "Yes" ? 1 : 0,
+          q2: sheet[i].activePOR === "Yes" ? 1 : 0,
+          q3: sheet[i].redemption === "Yes" ? 1 : 0,
+          entryGoogle50KSpacing: sheet[i].entry50kFacing,
+          entryGoogle100KSpacing: sheet[i].entry100kFacing,
+          entryGoogle150KSpacing: sheet[i].entry150kFacing,
+          entryGoogle300KSpacing: sheet[i].entry300kFacing,
+          entryGoogle500KSpacing: sheet[i].entry500kFacing,
+          exitGoogle50KSpacing: sheet[i].exit50kFacing,
+          exitGoogle100KSpacing: sheet[i].exit100kFacing,
+          exitGoogle150KSpacing: sheet[i].exit150kFacing,
+          exitGoogle300KSpacing: sheet[i].exit300kFacing,
+          exitGoogle500KSpacing: sheet[i].exit500kFacing,
+        }
+
+        await tbl_visits.create(newData)
+          .then(response => { })
+          .catch(err => { })
+      }
+      console.log("FINISH IMPORT VISIT", createDateAsUTC(new Date()))
+
+    } catch (err) {
+      console.log(err)
+      res.status(500).json({ message: "Error", err })
     }
   }
 }
--- routes/visit.js
+++ routes/visit.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
 const visitController = require('../controllers/visit')
 const { authentication } = require('../middlewares/auth')
-const { uploadAny } = require('../middlewares/multer')
+const { uploadAny, uploadSingle } = require('../middlewares/multer')
 
 router.use(authentication)
 router.post('/', uploadAny.any(), visitController.create)
 router.get('/', visitController.findAll)
+router.post('/import', uploadSingle.single('files'), visitController.import)
 router.get('/:id', visitController.findOne)
 router.put('/:id', uploadAny.any(), visitController.update)
 router.delete('/:id', visitController.delete)
